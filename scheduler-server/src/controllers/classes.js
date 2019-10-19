b922c4162a6b83e625b939f6f55b267cc44419d9
Brian Wheelhouse <bswheel@msn.com>
2019-10-18 21:20:20 -0600

    pushing server files

---
+++ scheduler-server/src/controllers/classes.js
@@ -0,0 +1,6 @@
+'using strict'
+
+exports.getClasses = function () {
+    console.log('entering getClasses()')
+    return { message: "getClass has been called."}
+}
---
+++ scheduler-server/src/controllers/scheduler.js
@@ -0,0 +1,6 @@
+'using strict'
+
+exports.createSchedules = function () {
+    console.log('entering createSchedule()')
+    return { message: 'createSchedule has been called.' }
+}
