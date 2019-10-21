95ab2e3b6155a58a9f2650278b0b084172efee74
Branden Foley <brandenfoley@gmail.com>
2019-10-21 09:22:23 -0600

    changed calendar display not show sunday, have min/max times, added class selector dropdowns

---
+++ startbootstrap-sb-admin-2-gh-pages/main.js
@@ -0,0 +1,11 @@
+function setTerm(val){
+    console.log("term set")
+}
+
+function setSubject(val){
+    console.log("term set")
+}
+
+function setCourse(val){
+    console.log("course set: " + val)
+}
