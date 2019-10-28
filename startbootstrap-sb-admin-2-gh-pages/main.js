83844a8bfda5eba64b2c65f954c5e4a431661c25
Branden Foley <brandenfoley@gmail.com>
2019-10-28 08:59:08 -0600

    set up class list, adding/removing classes

--- startbootstrap-sb-admin-2-gh-pages/main.js
+++ startbootstrap-sb-admin-2-gh-pages/main.js
@@ -1,11 +1,199 @@
-function setTerm(val){
-    console.log("term set")
+// Need to know formats of different things is next step. everything else looks good!
+var rowCount = 0
+var term = ""
+var sub = ""
+var course = ""
+var courseIndex = ""
+var courseFullName = ""
+
+function setTerm() {
+    t = document.getElementById("term");
+    term = t.options[t.selectedIndex].text;
+    //maybe use this for something?
+
+    s = document.getElementById("sub");
+    s.disabled = false;
+    s.innerHTML = "<option>--Select Course--</option>"; //empty (if they change the list)
+    
+    var list = document.getElementById("sub");
+    for (var i in subs) {
+        list.add(new Option(subs[i], subs[i]));
+    }
 }
 
-function setSubject(val){
-    console.log("term set")
+function setSubject() {
+    s = document.getElementById("sub");
+    sub = s.options[s.selectedIndex].text;
+    // update courses of subject
+    document.getElementById("course").disabled = false;
+    document.getElementById("course").innerHTML = "<option>--Select Course--</option>"; //empty (if they change the list)
+    var c = document.getElementById("course");
+    for(var i in myJson["classes"]){ //pull all of these that work
+        //match to subject
+        if(myJson["classes"][i]["department"] == sub){
+            c.add(new Option(myJson["classes"][i]["classId"], myJson["classes"][i]["classId"]));
+        }
+    }
 }
 
-function setCourse(val){
-    console.log("course set: " + val)
+function setCourse() {
+    c = document.getElementById("course");
+    course = c.options[c.selectedIndex].text;
+    //get the full name of course.
+
+    for(var i in myJson["classes"]){ //pull all of these that work
+        //match to subject
+        if(myJson["classes"][i]["classId"] == course){
+            courseFullName = myJson["classes"][i]["fullTitle"];
+            break;
+        }
+    }
+}
+ 
+//if already in there, dont add.
+function addClass() {
+    var table = document.getElementById("myTable");
+    if(classAlreadyExists()){return;}
+    var row = table.insertRow(-1);
+    row.setAttribute("id", rowCount);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+
+    //need a list of the actual subject names, not just abbreviations
+    s = document.getElementById("sub");
+    c = document.getElementById("course");
+    cell1.innerHTML = s.options[s.selectedIndex].text;
+    cell2.innerHTML = c.options[c.selectedIndex].text;
+    cell3.innerHTML = courseFullName;
+    cell4.innerHTML = '<i class="far fa-trash-alt" onclick="dropClass('+rowCount+');"></i>';
+    rowCount++;
+}
+
+function classAlreadyExists(){
+    var table = document.getElementById("myTable");
+    tableLength = table.rows.length;
+    for (let i = 1; row = table.rows[i]; i++) {
+        //only need second col of each row. skip header
+        if(row.cells[2].innerHTML == courseFullName){
+            return true;
+        }
+    }
+    return false;
+}
+
+//remove from table.
+function dropClass(rowId) {
+    var row = document.getElementById(rowId);
+    row.parentElement.removeChild(row);
+}
+
+//package things up and send to backend
+function submitList() {
+    var classJson = "{'classesToAdd': [";
+    var table = document.getElementById("myTable");
+    tableLength = table.rows.length;
+    console.log(tableLength);
+    for (let i = 1; row = table.rows[i]; i++) {
+        //only need second col of each row. skip header
+        classJson += "{'classId':'"+row.cells[1].innerHTML+"'},";
+    }
+    classJson = classJson.slice(0, classJson.length-1); //chop the last comma
+    classJson += "}";
+    console.log("json: " + classJson);
+    return classJson;
+}
+
+function getClassList(){
+    console.log("getting list")
+}
+
+var subs =['A HTG', 'ACC', 'AEROS', 'AFRIK', 'AM ST', 'ANES', 'ANTHR', 'ARAB', 'ARMEN', 'ART', 'ARTED', 'ARTHC', 'ASIAN', 'ASL', 'BIO', 'BULGN', 'C S', 'CAMBO', 'CANT', 'CE EN', 'CEBU', 'CFM', 'CH EN', 'CHEM', 'CHIN', 'CL CV', 'CLSCS', 'CMLIT', 'CMPST', 'COMD', 'COMMS', 'CPSE', 'CREOL', 'CSANM', 'DANCE', 'DANSH', 'DES', 'DESAN', 'DESGD', 'DESIL', 'DESPH', 'DIGHT', 'DUTCH', 'EC EN', 'ECE', 'ECON', 'EDLF', 'EIME', 'EL ED', 'ELANG', 'ENG T', 'ENGL', 'ENT', 'ESL', 'ESTON', 'EUROP', 'EXDM', 'EXSC', 'FHSS', 'FIN', 'FINN', 'FLANG', 'FNART', 'FREN', 'GEOG', 'GEOL', 'GERM', 'GREEK', 'GSCM', 'GWS', 'HAWAI', 'HCOLL', 'HEB', 'HINDI', 'HIST', 'HLTH', 'HMONG', 'HONRS', 'HRM', 'HUNG', 'IAS', 'ICLND', 'ICS', 'IHUM', 'INDES', 'INDON', 'IP&T', 'IS', 'IT&C', 'ITAL', 'JAPAN', 'KICHE', 'KIRIB', 'KOREA', 'LATIN', 'LATVI', 'LAW', 'LFSCI', 'LING', 'LINGC', 'LITHU', 'LT AM', 'M COM', 'MALAG', 'MALAY', 'MATH', 'MBA', 'ME EN', 'MESA', 'MFGEN', 'MFHD', 'MFT', 'MIL S', 'MKTG', 'MMBIO', 'MPA', 'MSB', 'MTHED', 'MUSIC', 'NAVAJ', 'NDFS', 'NE LG', 'NEURO', 'NORWE', 'NURS', 'PDBIO', 'PERSI', 'PETE', 'PHIL', 'PHSCS', 'PHY S', 'PLANG', 'POLI', 'POLSH', 'PORT', 'PSYCH', 'PWS', 'QUECH', 'REL A', 'REL C', 'REL E', 'ROM', 'RUSS', 'SAMOA', 'SC ED', 'SCAND', 'SFL', 'SLAT', 'SLOVK', 'SOC', 'SOC W', 'SPAN', 'STAC', 'STAT', 'STDEV', 'STRAT', 'SWAHI', 'SWED', 'SWELL', 'T ED', 'TAGAL', 'TECH', 'TEE', 'TELL', 'TES', 'TEST', 'THAI', 'TMA', 'TONGA', 'TRM', 'TURK', 'UNIV', 'URDU', 'VIET', 'WELSH', 'WRTG']
+var myJson = {
+    "classes": [
+        {
+            "classId": "CS142",
+            "department": "C S",
+            "courseNumber": "142",
+            "title": "Intro to Computer Programming",
+            "fullTitle": "Introduction to Computer Programming"
+        },
+        {
+            "classId": "CS224",
+            "department": "C S",
+            "courseNumber": "224",
+            "title": "Computer Systems",
+            "fullTitle": "Introduction to Computer Systems"
+        },
+        {
+            "classId": "CS235",
+            "department": "C S",
+            "courseNumber": "235",
+            "title": "Data Structures",
+            "fullTitle": "Data Structures and Algorithms"
+        },
+        {
+            "classId": "MATH102",
+            "department": "MATH",
+            "courseNumber": "102",
+            "title": "Quantitative Reasoning",
+            "fullTitle": "Quantitative Reasoning"
+        },
+        {
+            "classId": "MATH110",
+            "department": "MATH",
+            "courseNumber": "110",
+            "title": "College Algebra",
+            "fullTitle": "College Algebra"
+        },
+        {
+            "classId": "MATH111",
+            "department": "MATH",
+            "courseNumber": "111",
+            "title": "Trigonometry",
+            "fullTitle": "Trigonometry"
+        },
+        {
+            "classId": "SWELL105",
+            "department": "SWELL",
+            "courseNumber": "105",
+            "title": "Pickleball",
+            "fullTitle": "Pickleball"
+        }
+    ],
+    "10178-002": {
+        "year_term": "20195",
+        "curriculum_id": "10178",
+        "title_code": "002",
+        "dept_name": "C S",
+        "catalog_number": "224",
+        "catalog_suffix": null,
+        "title": "Computer Systems",
+        "full_title": "Introduction to Computer Systems.",
+        "sections": [
+            {
+                "section_number": "001",
+                "fixed_or_variable": "F",
+                "credit_hours": "3.00",
+                "minimum_credit_hours": "3.00",
+                "honors": null,
+                "credit_type": "S",
+                "section_type": "DAY",
+                "instructor_name": null,
+                "instructor_id": null
+            },
+            {
+                "section_number": "002",
+                "fixed_or_variable": "F",
+                "credit_hours": "3.00",
+                "minimum_credit_hours": "3.00",
+                "honors": null,
+                "credit_type": "S",
+                "section_type": "DAY",
+                "instructor_name": null,
+                "instructor_id": null
+            }
+        ]
+    }
 }
