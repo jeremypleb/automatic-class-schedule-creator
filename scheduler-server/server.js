1c5a6d176ea07256bc71e3258c79cf698e5d8478
Brian Wheelhouse <bswheel@msn.com>
2019-11-04 06:49:49 -0700

    pseudo code and mocking

--- scheduler-server/server.js
+++ scheduler-server/server.js
@@ -21,14 +21,14 @@ const controllersPath =  path.resolve(__dirname, './src/controllers')
 //create an enforcer instance
 const enforcer = EnforcerMiddleware(swaggerPath)
 
-/*//check for explicit mock request
-enforcer.mocks({}, false).catch(console.error)*/
+//check for explicit mock request
+enforcer.mocks({}, false).catch(console.error)
 
 //call defined operation handlers
 enforcer.controllers(controllersPath).catch(console.error)
 
-/*//produce fallback mock responses
-enforcer.mocks({}, true).catch(console.error)*/
+//produce fallback mock responses
+enforcer.mocks({}, true).catch(console.error)
 
 //tell express to run the internal open api enforcer middleware
 app.use(enforcer.middleware())
