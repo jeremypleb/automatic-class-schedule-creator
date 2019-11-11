b57392f9032d255bc8ab909ddd907508a49e2561
Jeremy Hodges <jeremypleb@gmail.com>
2019-11-11 12:47:01 -0700

    wip

--- scheduler-server/src/controllers/scheduler.js
+++ scheduler-server/src/controllers/scheduler.js
@@ -5,6 +5,10 @@ import { Scheduler } from '../services/scheduler';
 exports.createSchedules = function (req, res) {
     console.log('entering createSchedule()');
 
+    const courseIds = [];
+    const blockedTime; [];
+    const dirPath = '../../../data/20195';
+
     // TODO we can format the validSchedules and send back the response as the client expects
     const scheduler = new Scheduler(courseIds, blockedTime, dirPath);
     const validSchedules = scheduler.generateSchedules(5);
