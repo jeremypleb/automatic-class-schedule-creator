c08cec785c07a3fc428010f6d8569179a064f64e
Branden Foley <brandenfoley@gmail.com>
2019-11-12 22:06:21 -0700

    locking term after class is added

--- startbootstrap-sb-admin-2-gh-pages/main.js
+++ startbootstrap-sb-admin-2-gh-pages/main.js
@@ -12,13 +12,12 @@ var sourceObject = 1
 function setTerm() {
     t = document.getElementById("term");
     term = t.options[t.selectedIndex].text;
-    //maybe use this for something?
 
     s = document.getElementById("sub");
     s.disabled = false;
-    s.innerHTML = "<option>--Select Course--</option>"; //empty (if they change the list)
+    s.innerHTML = "<option>--Select Subject--</option>"; //empty (if they change the list)
+    document.getElementById("course").innerHTML =  "<option>--Select Course--</option>"; //empty (if they change the list)
 
-    //I pulled all of the subjects from the json and put them in a list here.
     var list = document.getElementById("sub");
     for (var i in subs) {
         list.add(new Option(subs[i], subs[i]));
@@ -52,6 +51,8 @@ function setCourse() {
             break;
         }
     }
+    // lock term
+    document.getElementById("term").disabled = true;
 }
 
 //if already in there, dont add.
@@ -91,6 +92,10 @@ function classAlreadyExists(){
 function dropClass(rowId) {
     var row = document.getElementById(rowId);
     row.parentElement.removeChild(row);
+
+    // if all gone unlock the term
+    t = document.getElementById("term");
+    t.disabled = false;
 }
 
 //package things up and send to backend
@@ -101,8 +106,6 @@ function buildList() {
     tableLength = table.rows.length;
     console.log(tableLength);
     for (let i = 1; row = table.rows[i]; i++) {
-        //only need second col of each row. skip header
-            
         classJson += "{'classId':'"+row.cells[1].innerHTML+"'},";
     }
     classJson = classJson.slice(0, classJson.length-1); //chop the last comma
@@ -140,7 +143,8 @@ function getClassList(){
 
 //get schedules from backend, and add to calendar.
 function getSchedule() {
-    
+    schedules = buildList();
+    console.log(schedules);
 }
 
 var subs =['A HTG', 'ACC', 'AEROS', 'AFRIK', 'AM ST', 'ANES', 'ANTHR', 'ARAB', 'ARMEN', 'ART', 'ARTED', 'ARTHC', 'ASIAN', 'ASL', 'BIO', 'BULGN', 'C S', 'CAMBO', 'CANT', 'CE EN', 'CEBU', 'CFM', 'CH EN', 'CHEM', 'CHIN', 'CL CV', 'CLSCS', 'CMLIT', 'CMPST', 'COMD', 'COMMS', 'CPSE', 'CREOL', 'CSANM', 'DANCE', 'DANSH', 'DES', 'DESAN', 'DESGD', 'DESIL', 'DESPH', 'DIGHT', 'DUTCH', 'EC EN', 'ECE', 'ECON', 'EDLF', 'EIME', 'EL ED', 'ELANG', 'ENG T', 'ENGL', 'ENT', 'ESL', 'ESTON', 'EUROP', 'EXDM', 'EXSC', 'FHSS', 'FIN', 'FINN', 'FLANG', 'FNART', 'FREN', 'GEOG', 'GEOL', 'GERM', 'GREEK', 'GSCM', 'GWS', 'HAWAI', 'HCOLL', 'HEB', 'HINDI', 'HIST', 'HLTH', 'HMONG', 'HONRS', 'HRM', 'HUNG', 'IAS', 'ICLND', 'ICS', 'IHUM', 'INDES', 'INDON', 'IP&T', 'IS', 'IT&C', 'ITAL', 'JAPAN', 'KICHE', 'KIRIB', 'KOREA', 'LATIN', 'LATVI', 'LAW', 'LFSCI', 'LING', 'LINGC', 'LITHU', 'LT AM', 'M COM', 'MALAG', 'MALAY', 'MATH', 'MBA', 'ME EN', 'MESA', 'MFGEN', 'MFHD', 'MFT', 'MIL S', 'MKTG', 'MMBIO', 'MPA', 'MSB', 'MTHED', 'MUSIC', 'NAVAJ', 'NDFS', 'NE LG', 'NEURO', 'NORWE', 'NURS', 'PDBIO', 'PERSI', 'PETE', 'PHIL', 'PHSCS', 'PHY S', 'PLANG', 'POLI', 'POLSH', 'PORT', 'PSYCH', 'PWS', 'QUECH', 'REL A', 'REL C', 'REL E', 'ROM', 'RUSS', 'SAMOA', 'SC ED', 'SCAND', 'SFL', 'SLAT', 'SLOVK', 'SOC', 'SOC W', 'SPAN', 'STAC', 'STAT', 'STDEV', 'STRAT', 'SWAHI', 'SWED', 'SWELL', 'T ED', 'TAGAL', 'TECH', 'TEE', 'TELL', 'TES', 'TEST', 'THAI', 'TMA', 'TONGA', 'TRM', 'TURK', 'UNIV', 'URDU', 'VIET', 'WELSH', 'WRTG']
